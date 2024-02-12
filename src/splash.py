b0f849fbaa99a9ee17b9e49b6167ad4a8f9de39e
Pranav Mahesh <102569626+pmahesh29@users.noreply.github.com>
2024-02-12 23:39:06 +0000

    Testing Splash Screen

--- src/splash.py
+++ src/splash.py
@@ -14,4 +14,16 @@ def build(root: tk.Tk) -> tk.Label:
 
     # Make background black
     splash_screen.configure(background="black")
+    
+    # Build the splash screen
+    splash: splash_screen = splash_screen.build(root)
+
+    # After 3 seconds, destroy the splash screen and build the player entry screen
+    # Play action screen will be built after F5 is pressed on player entry screen (see on_f5 function in src/player_entry.py)
+    root.after(3000, splash.destroy)
+    root.after(3000, player_entry.build, root, users, network)
+
+
+if __name__ == "__main__":
+    main()
     return splash_screen

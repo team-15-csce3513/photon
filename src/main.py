635e1365f6e1a6d8d0ecf4a2d4da2225e42813c0
Pranav Mahesh <102569626+pmahesh29@users.noreply.github.com>
2024-02-25 17:00:23 -0600

    Update main.py
    Edited the main.py to implement the new splash + entry screen

--- src/main.py
+++ src/main.py
@@ -18,29 +18,31 @@ print("SUPABASE_KEY:", os.getenv("SUPABASE_KEY"))
 supabase_client = initialize_supabase()
 
 def main():
-    root = tk.Tk()
-
-    def close_root(event):
-        root.destroy()
-
-    root.title("Main Application")
-    root.geometry("800x600")
-    root.configure(bg="black")
-
-    root.bind('<Escape>', close_root)
+    window = tk.Tk()
+    window.title("Splash Screen")
+    window.geometry("800x600")
 
     # Build and display splash screen
-    splash_screen = build_splash(root)
-    root.update()
-
-    # Simulate some processing time
+    splash_screen = build_splash(window)
+    window.update()
     time.sleep(2)
 
-    # Destroy the splash screen
     splash_screen.destroy()
+
+
+    # window.bind("Enter", splash_screen)
+    
+    window.destroy()
+    # Simulate some processing time
+
+    # Destroy the splash screen
+
+    # Call the player entry screen
+
 
     # Call the player entry screen
     create_player_entry_screen(supabase_client)
+
 
     # Run UDP client and server in separate threads
     # client_thread = threading.Thread(target=udp_client)
@@ -53,13 +55,7 @@ def main():
     # client_thread.join()
     # server_thread.join()
 
-    root.mainloop()
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
- 
-
-
-if __name__ == "__main__":
-    main()
- 

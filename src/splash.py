84a26d6ba66cf9e6ffa51c44d90811d77ff1548e
Pranav Mahesh <102569626+pmahesh29@users.noreply.github.com>
2024-03-17 21:37:04 -0500

    Update splash.py (#23)

--- src/splash.py
+++ src/splash.py
@@ -1,27 +1,12 @@
 from PIL import Image, ImageTk
 import tkinter as tk
 
-# Build the splash screen, destroy after 3 seconds
-def build(root: tk.Tk) -> tk.Label:
-    # Load the splash screen image
-    splash_pil_image = Image.open("../images/splash.jpg")
+# After 3 seconds, destroy the splash screen
+def build(window: tk.Tk, duration_ms: int = 3000) -> tk.Label:
+    splash_pil_image = Image.open("../assets/images/splash.jpg")
     splash_image = ImageTk.PhotoImage(splash_pil_image)
-    
+
     # Build the splash screen
-    splash_screen = tk.Label(root, image=splash_image)
+    splash_screen = tk.Label(window, image=splash_image, bg='black')
     splash_screen.place(x=0, y=0, relwidth=1, relheight=1)
     splash_screen.image = splash_image
-
-    # Make background black
-    splash_screen.configure(background="black")
-
-    return splash_screen
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.geomentry("800x600")
-    splash_screen = build(root)
-
-    # After 3 seconds, destroy the splash screen
-    root.after(3000, splash_screen.destroy)
-    root.mainloop()

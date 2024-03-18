1b6937ff91588358fdc7c210fdd66092e7c46f3a
Pranav Mahesh <102569626+pmahesh29@users.noreply.github.com>
2024-03-17 22:04:26 -0500

    Update supabase_config.py (#24)

--- src/supabase_config.py
+++ src/supabase_config.py
@@ -2,21 +2,13 @@ import os
 from dotenv import load_dotenv
 import supabase
 
+
 # Load environment variables from .env file
 load_dotenv()
 
-# Print loaded environment variables (for debugging)
-print("SUPABASE_URL:", os.getenv("SUPABASE_URL"))
-print("SUPABASE_KEY:", os.getenv("SUPABASE_KEY"))
-
 # Function to initialize Supabase client
 def initialize_supabase():
-    supabase_client = supabase.create_client(
+    supabase_client: supabase.Client = supabase.create_client(
         os.getenv("SUPABASE_URL"),
-        os.getenv("SUPABASE_KEY")
-    )
-
-    # Print Supabase client details (for debugging)
-    print("Supabase Client:", supabase_client)
-
+        os.getenv("SUPABASE_KEY"))
     return supabase_client
